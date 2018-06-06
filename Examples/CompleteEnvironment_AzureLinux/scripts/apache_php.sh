51fe011f4384da2acd7009c7017cff9011f6b68f
solkeee <39927516+solkeee@users.noreply.github.com>
2018-06-06 14:13:34 +0100

    Update apache_php.sh

--- Examples/CompleteEnvironment_AzureLinux/scripts/apache_php.sh
+++ Examples/CompleteEnvironment_AzureLinux/scripts/apache_php.sh
@@ -3,7 +3,7 @@ export DEBIAN_FRONTEND=noninteractive
 sudo apt-get -y update && sudo apt-get -y upgrade
 sudo apt-get -y install apache2
 pub_ip=$(wget -qO- http://ipecho.net/plain)
-sudo bash -c "echo 'ServerName $pub_ip' >> /etc/apache2/apache2.conf"
+sudo bash -c "echo 'ServerName $pub_ip' | dd of=/etc/apache2/apache2.conf"
 sudo service apache2 restart
 sudo add-apt-repository ppa:ondrej/php -y && sudo apt-get -y update
 sudo apt install -y php7.1 libapache2-mod-php7.1 php7.1-mysql php-common php7.1-cli php7.1-common php7.1-json php7.1-opcache php7.1-readline\
