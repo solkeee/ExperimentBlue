5acb72ed4ca6354e7f6141735ca0aa59361eb5ed
solkeee <39927516+solkeee@users.noreply.github.com>
2018-06-27 16:21:38 +0100

    updated to add bind address and service user

--- Examples/CompleteEnvironment_AzureLinux/scripts/mysql_install.sh
+++ Examples/CompleteEnvironment_AzureLinux/scripts/mysql_install.sh
@@ -26,11 +26,27 @@ expect \"Reload privilege tables now?\"
 send \"y\r\"
 expect eof
 ")
-
-echo "$SECURE_MYSQL"
 sudo su
+echo "$SECURE_MYSQL"
+echo "Updating mysql configs in /etc/mysql/my.cnf."
+sudo sed -i "s/.*bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/my.cnf
+echo "Updated mysql bind address in /etc/mysql/my.cnf to 0.0.0.0 to allow external connections."
+
+echo "Creating Db test ..."
 sudo mysql -uroot -e "CREATE DATABASE test;"
+echo "Creation of Db test complete."
+
+echo "Assigning mysql user dbcon access on %."
+sudo mysql -u dbcon -pdbconpwd2018 --execute "GRANT ALL PRIVILEGES ON *.* TO 'dbcon'@'%' IDENTIFIED BY 'dbconpwd2018' with GRANT OPTION; FLUSH PRIVILEGES;" test
+echo "Assigned mysql user dbcon access on all hosts."
+
+sudo service mysql stop
+sudo service mysql start
+
+
+#sudo su
+#sudo mysql -uroot -e "CREATE DATABASE test;"
 #sudo mysql -e "CREATE USER 'demouser'@'localhost' IDENTIFIED BY 'demopwd';"
-sudo mysql -e "GRANT ALL PRIVILEGES ON * . * TO 'root'@'localhost';"
+#sudo mysql -e "GRANT ALL PRIVILEGES ON * . * TO 'root'@'%';"
 #sudo mysql -e "FLUSH PRIVILEGES;"
 apt-get purge -y expect
